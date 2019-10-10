d0596800ffd3287e4355cff29e8cfc610d2e33e5
Erik Anderson <erik@ayarlabs.com>
2019-10-09 17:45:47 -0700

    Scan gen module almost complete

--- scan_generator/src/ScanGenerator.scala
+++ scan_generator/src/ScanGenerator.scala
@@ -8,6 +8,9 @@ import java.nio.file.{Paths, Files}
 // TODO Add CLI parser 
 // TODO Generate definitions in verilog
 // TODO Create a map data structure for accessing cellIn and cellOut in chisel modules
+// TODO generate verilog tasks for testing scan chains  
+// TODO generate timing sdc commands   
+// TODO implement mult   
 
 ///////////////////////////////////////////////////////////
 // Define YAML schema
@@ -39,31 +42,72 @@ case class ScanChainParameters (
  *  Description: Connects scan chain cells to form scan chain elements 
  *  @param p ScanCellParameters. See defined case class
  *  */
-  class ScanChainGenerator(p: ScanChainParameters) {
-    println(s"$p")
   //class ScanChainGenerator(p: ScanChainParameters) extends RawModule {
-  //var readCtr = 0
-  //var writeCtr = 0
-  //inWidth,outWidth = calculateIOWidths(p)
-  //val io = IO(new Bundle(){
-  //  // Scan in and out
-  //  val scanIn = Input(Bool())
-  //  val scanOut = Output(Bool())
-  //  val out = if (!p.write) Some(Input(UInt(p.width.W))) else None
-  //  val cellOut = if (p.write) Some(Output(UInt(p.width.W))) else None
-  //  // Reset - resets the output stage only
-  //  val scanReset = if (p.update) Some(Input(Bool())) else None
-  //  // Clocks 
-  //  val scanClk = if (!p.twoPhase) Some(Input(Clock())) else None
-  //  val scanClkP = if (p.twoPhase) Some(Input(Clock())) else None
-  //  val scanClkN = if (p.twoPhase) Some(Input(Clock())) else None
-  //  // Control Signals
-  //  val scanEn = Input(Bool())
-  //  val scanUpdate = if (p.update) Some(Input(Bool())) else None
-  //})
+  class ScanChainGenerator(p: ScanChainParameters) {
+    // Calculate IO widths and filter cells
+    val writeCells = p.cells.filter(x => x.write == true)
+    val readCells = p.cells.filter(x => x.write == false)
+    val outWidth = writeCells.map(x => x.width).fold(0)((a,b) => a + b)
+    val inWidth = readCells.map(x => x.width).fold(0)((a,b) => a + b)
+    
+    //// IO declaration 
+    //val io = IO(new Bundle(){
+    //  // Scan in and out
+    //  val scanIn = Input(Bool())
+    //  val scanOut = Output(Bool())
+    //  // Parallel in and out
+    //  val out = if (outWidth != 0) Some(Output(UInt(outWidth.W))) else None
+    //  val in = if (inWidth != 0) Some(Input(UInt(inWidth.W))) else None
+    //  // Reset - resets the output stage only
+    //  val scanReset = if (p.cellType.scanupdate) Some(Input(Bool())) else None
+    //  // Clocks 
+    //  val scanClk = if (!p.cellType.twoPhase) Some(Input(Clock())) else None
+    //  val scanClkP = if (p.cellType.twoPhase) Some(Input(Clock())) else None
+    //  val scanClkN = if (p.cellType.twoPhase) Some(Input(Clock())) else None
+    //  // Control Signals
+    //  val scanEn = Input(Bool())
+    //  val scanUpdate = if (p.cellType.update) Some(Input(Bool())) else None
+    //})
 
-  //def calculateIOWidths(p: ScanChainParameters): (Int,Int) = {
-  //  (inWidth,outWidth)
-  //} 
- 
+    // Create all cells
+    val cells = p.cells.map(c => {
+      // Iterate over mult
+      for (i <- 0 until c.mult) { 
+        println(s"Created cell: ${c.name}, mult: $i")
+        //Module(new ScanCell(new ScanCellParameters(
+        //    width=c.width,
+        //    twoPhase=p.cellType.twoPhase,
+        //    write=c.write,
+        //    update=p.cellType.update,
+        //    updateLatch= p.cellType.updateLatch
+        //  )))
+      }
+    })
+    
+    //var readCtr = 0
+    //var writeCtr = 0
+    //// Connect scan cells
+    //p.cells.foreach(c => {
+    //  // Iterate over mult
+    //  
+    //  // Connect clocks
+    //  if (p.cellType.twoPhase) {
+    //    cell.io.scanClkP.get := io.scanClkP.get
+    //    cell.io.scanClkN.get := io.scanClkN.get
+    //  } else {
+    //    cell.io.scanClk := io.scanClk.get
+    //  }
+    //  // Connect in/out 
+    //  if (c.write) {
+    //    io.out(writeCtr+c.width-1,writeCtr) := cell.io.cellOut.get 
+    //    writeCtr += c.width
+    //  } else {
+    //    cell.io.cellIn.get := io.in(writeCtr+c.width-1,writeCtr)
+    //    readCtr += c.width
+    //  }
+    //  // Connect control
+    //  
+    //}) 
+    
+  
 }
