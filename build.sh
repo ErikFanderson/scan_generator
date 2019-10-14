627a2a77b907258ad84343392bdcbf99eef7ec8e
Erik Anderson <erik@ayarlabs.com>
2019-10-14 15:55:47 -0700

    updated build script

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-mvn install:install-file -DgroupId=ErikFanderson -DartifactId=scan_generator -Dversion=0.0.1 -Dfile=scan_generator-0.0.1.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+GROUP=ErikFanderson
+ARTIFACT=scan_generator
+VERSION=0.0.1
+JAR="../scan_generator-0.0.1.jar"
+mvn install:install-file -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dfile=$JAR -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
