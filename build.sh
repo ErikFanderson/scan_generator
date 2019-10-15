72becc818eb3e6078e3f3a0d79e3d6ed3ae2e2c2
Erik Anderson <erik@ayarlabs.com>
2019-10-14 17:03:53 -0700

    Added underscore scala version

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 GROUP=ErikFanderson
 ARTIFACT=scan_generator
-VERSION=0.0.1
-JAR="../scan_generator-0.0.1.jar"
-mvn install:install-file -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dfile=$JAR -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+SCALA_VERSION="2.11"
+VERSION="0.0.1"
+JAR="../out.jar"
+mvn install:install-file -DgroupId=$GROUP -DartifactId=${ARTIFACT}_${SCALA_VERSION} -Dversion=$VERSION -Dfile=$JAR -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
