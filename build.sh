1775efc3a41edb58af0384b2aa35110e10d52a37
Erik Anderson <erik@ayarlabs.com>
2019-10-14 20:28:01 -0700

    yaml protocol update to 0.0.1

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ GROUP=ErikFanderson
 ARTIFACT=scan_generator
 SCALA_VERSION="2.11"
 VERSION="0.0.1"
-JAR="../out.jar"
+JAR="../../chisel/scan_generator/out/scan_generator/assembly/dest/out.jar"
 mvn install:install-file -DgroupId=$GROUP -DartifactId=${ARTIFACT}_${SCALA_VERSION} -Dversion=$VERSION -Dfile=$JAR -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
